e10a34ef911a7babb9c23c5b12df617dfbbe2165
Riona IT <154750938+Rionadev@users.noreply.github.com>
2025-02-07 04:35:33 +0900

    update: page color

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://admin.caposgt.com/api',
+  // apiUrl: 'http://admin.caposgt.com/api',
+  apiUrl: 'http://192.168.149.211:3000/api',
+
 };
